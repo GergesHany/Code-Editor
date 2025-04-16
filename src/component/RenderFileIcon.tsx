04d306cc0c301e82c56020c1067ad8f3e617222d
GergesHany <gergeshany179@gmail.com>
2025-04-16 14:43:19 +0200

    improve the RenderFileIcon and write comments

--- src/component/RenderFileIcon.tsx
+++ src/component/RenderFileIcon.tsx
@@ -6,50 +6,72 @@ interface IProps {
   isOpen?: boolean;
 }
 
-const mapping = new Map();
-mapping.set('js', 'javascript');
-mapping.set('jsx', 'react');
-mapping.set('ts', 'typescript');
-mapping.set('tsx', 'react_ts');
+// Define the file extension to icon name mapping
+const FILE_ICON_MAPPING = new Map([
+  // JavaScript/TypeScript files
+  ['js', 'javascript'],
+  ['jsx', 'react'],
+  ['ts', 'typescript'],
+  ['tsx', 'react_ts'],
+  
+  // Image files
+  ['png', 'image'],
+  ['jpg', 'image'],
+  ['jpeg', 'image'],
+  ['gif', 'image'],
+  ['svg', 'image'],
+  
+  // Special folders
+  ['node_modules', 'folder-node'],
+  ['public', 'folder-public'],
+  ['src', 'folder-src'],
+  ['dist', 'folder-dist'],
+  ['components', 'folder-components'],
+  ['assets', 'folder-assets'],
+  
+  // Default folder
+  ['folder-default', 'folder-default'],
+  ['folder-default-open', 'folder-default-open'],
+  
+  // Other common files
+  ['html', 'html'],
+  ['css', 'css'],
+  ['json', 'json'],
+  ['md', 'markdown'],
+  ['icon', 'favicon'],
+]);
 
-mapping.set('png', 'image');
-mapping.set('jpg', 'image');
-mapping.set('gif', 'image');
-mapping.set('node_modules', 'folder-node');
-mapping.set('public', 'folder-public');
-mapping.set('src', 'folder-src');
-mapping.set('folder-default', 'folder-default');
-mapping.set('icon', 'facicon');
-
-mapping.set('html', 'html');
-mapping.set('css', 'css');
-mapping.set('json', 'json');
-
-
-
-const RenderFileIcon = ({filename, isFolder, isOpen}: IProps) => {
-  let extension = filename.split('.').pop(); // Get the file extension  
-
-  if (isFolder && !mapping.has(extension)) {
-    if (isOpen) {
-      extension = 'folder-default-open';
+/**
+ * Component for rendering appropriate file or folder icons
+ * @param {string} filename - Name of the file or folder
+ * @param {boolean} isFolder - Whether the item is a folder
+ * @param {boolean} isOpen - Whether the folder is open (only applicable for folders)
+ */
+const RenderFileIcon = ({ filename, isFolder, isOpen }: IProps) => {
+  // Extract the extension or use the filename for special folders
+  const getIconKey = () => {
+    if (isFolder) {
+      // Check if it's a special folder we have an icon for
+      const folderName = filename.toLowerCase();
+      if (FILE_ICON_MAPPING.has(folderName)) {
+        return isOpen ? `${FILE_ICON_MAPPING.get(folderName)}-open`: FILE_ICON_MAPPING.get(folderName);
+      }
+      
+      // Default folder icon
+      return isOpen ? 'folder-default-open' : 'folder-default';
     }
-    else {
-      extension = 'folder-default';
-    }
-  }else{
-    extension = mapping.get(extension);
-  }
+    
+    // For files, get the extension
+    const extension = filename.split('.').pop()?.toLowerCase() || '';
+    
+    // Return the mapped icon name or the extension itself if not found
+    return FILE_ICON_MAPPING.get(extension) || extension;
+  };
 
-  if (isFolder && isOpen) {
-    return (
-      <IconImg src={`/public/icons/${extension}-open.svg`} />
-    );
-  }
+  const iconKey = getIconKey();
+  const iconPath = `/public/icons/${iconKey}.svg`;
 
-  return (
-    <IconImg src={`/public/icons/${extension}.svg`} />
-  );
+  return <IconImg src={iconPath} />;
 };
 
 export default RenderFileIcon;
