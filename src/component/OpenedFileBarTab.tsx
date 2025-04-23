ae7e243a0ddfea8a3ed0e8398d997707dd78b94f
GergesHany <gergeshany179@gmail.com>
2025-04-23 23:20:04 +0200

    added Remove Tab Handler

--- src/component/OpenedFileBarTab.tsx
+++ src/component/OpenedFileBarTab.tsx
@@ -5,6 +5,7 @@ import { setClikedFile } from "../app/features/fileTreeSlice";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { RootState } from "../app/store";
+import { setOpenedFiles } from "../app/features/fileTreeSlice";
 
 interface IProps {
   file: IFile;
@@ -13,10 +14,13 @@ interface IProps {
 const OpenedFileBarTab = ({file}: IProps) => {
 
   const { 
+    openedFiles,
     clickedFile: {activeTabId},
    } = useSelector((state: RootState) => state.tree);
 
   const dispatch = useDispatch();
+
+
   const onClick = () => {
     const {name, content, id} = file;
     dispatch(setClikedFile({
@@ -26,13 +30,39 @@ const OpenedFileBarTab = ({file}: IProps) => {
     }));
   }
 
+  const onRemove = (id: string) => {
+    const updatedFiles = openedFiles.filter((file) => file.id !== id);
+    dispatch(setOpenedFiles(updatedFiles));
+    if (updatedFiles.length > 0) {
+      const {id, name, content} = updatedFiles[updatedFiles.length - 1];
+      dispatch(setClikedFile({
+        activeTabId: id || "",
+        filename: name || "",
+        fileContent: content || "",
+      }));
+    }else{
+      // If no files are opened, set the clicked file to default values
+      dispatch(setClikedFile({ activeTabId: "", filename: "", fileContent: "", }));
+    }
+  }
+
   return (
     <div className={`flex items-center p-2 ${file.id === activeTabId ? "border-t-2 border-purple-500" : "border-t-2 border-transparent"}`} onClick={onClick}>
+      
       <RenderFileIcon filename={file.name} />
-      <span className="cursor-pointer duration-300 flex justify-center items-center w-fit mx-2 p-1 rounded-md">
-          {file.name}
-      </span>
-      <CloseIcon />
+        <span className="cursor-pointer duration-300 flex justify-center items-center w-fit mx-2 p-1 rounded-md">
+            {file.name}
+        </span>
+        <span 
+          className="cursor-pointer hover:bg-[#64646473] duration-300 flex justify-center items-center w-fit mr-2 p-1 rounded-md"
+          onClick={(e) => {
+            e.stopPropagation()
+            onRemove(file.id)}
+          }
+        >
+           <CloseIcon />
+        </span>
+
     </div>
   );
 };
